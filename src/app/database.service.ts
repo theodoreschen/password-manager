42061e6a18363a87816e95486e4e17068dc1a51b
theodoreschen <theodoreschen@gmail.com>
2020-09-17 23:23:02 -0400

    adding database.service

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { PasswordEntry } from './password-entry';
+import { backendUrl } from './urls';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  private backendUrl = backendUrl;
+  private jsonHttpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
   constructor(
     private http: HttpClient
   ) { }
 
-  
+  updateRecord (record: PasswordEntry): Observable<any> {
+    return this.http.post(`${this.backendUrl}/update`, record, this.jsonHttpOptions)
+      .pipe(
+        catchError(this.handleError<any>("DatabaseService.updateRecord"))
+      );
+  }
+
+  getAllRecords (): Observable<PasswordEntry[]> {
+    return this.http.get<PasswordEntry[]>(`${this.backendUrl}/getall`)
+      .pipe(
+        catchError(this.handleError<PasswordEntry[]>("DatabaseService.getAllRecords"))
+      );
+  }
 
   private handleError<T> (operation="operation", result?: T) {
     return (error: any): Observable<T> => {
